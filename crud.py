c42600d81dd05722c70248949361e38768d19db7
mstp <EHStudent230@hotmail.com>
2024-03-25 20:56:18 -0400

    ajout menu

--- crud.py
+++ crud.py
@@ -36,9 +36,38 @@ def saisie():
     email=input("Entrez votre adresse e-mail : ")
     create_user(nom, email)
 
+def menu():
+    while True:
+        print("\nMenu:")
+        print("1. Ajouter un utilisateur")
+        print("2. Afficher les utilisateurs")
+        print("3. Mettre à jour un utilisateur")
+        print("4. Supprimer un utilisateur")
+        print("5. Quitter")
+        choix = input("Entrez votre choix (1-5): ")
 
+        if choix == '1':
+            nom = input("Entrez votre nom complet : ")
+            email = input("Entrez votre adresse e-mail : ")
+            create_user(nom, email)
+        elif choix == '2':
+            read_users()
+        elif choix == '3':
+            id = input("Entrez l'ID de l'utilisateur à mettre à jour : ")
+            email = input("Entrez le nouvel email : ")
+            update_user(id, email)
+        elif choix == '4':
+            id = input("Entrez l'ID de l'utilisateur à supprimer : ")
+            delete_user(id)
+        elif choix == '5':
+            print("Au revoir !")
+            break
+        else:
+            print("Choix non valide. Veuillez réessayer.")
+
+menu()
 #create_user("John Dope", "john.dope@example.com")
-read_users()
+#read_users()
 #update_user(2, 'emicheldev@gmail.com')
 #delete_user(2)
 #saisie()
